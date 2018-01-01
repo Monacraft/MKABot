42452edc6bf2c64a132852f26853a5854e9174ae
unknown <monacrafter@gmail.com>
2018-01-01 13:45:47 +1100

    Changed from react to private message with confirmation text

--- bot.js
+++ bot.js
@@ -6,8 +6,9 @@ const client = new Discord.Client();
 var myID = '396672130479161354';
 var devID = '130568487679688704';
 var fs = require('fs');
-var welcomeChannel = '389198025530015754'; // devtest ='396668459053744131';
-var guildID = '237582214525616129'; // devtest = '396668459053744128';
+var welcomeChannel = ''; // actual = '389198025530015754'; // devtest ='396668459053744131';
+var guildID = ''; // actual = '237582214525616129'; // devtest = '396668459053744128';
+var lastMessageID = '';
 var started = 0;
 var oldMessage = '';
 var shutdown = false;
@@ -17,14 +18,20 @@ var rolePendingName = 'Membership Pending';
 var roleAccept = '';
 var roleAcceptName = 'MKA Member';
 var accept = '✅';
+var fail = '❌';
 var notAccepted = [];
+var kickedCount = 0;
+var acceptedCount = 0;
 
+var acceptText = "I will not ask or offer medical advice";
+var acceptMsg = accept + " Congrats! You're now a member of the discord"
+var failMsg = fail + ' Error... Please type letter for letter: `' + acceptText + '`';
 var welcomeText = `
 __** Important Information: **__
 
 You have 10 minutes before you can interact with the members.
 Please take this time to read through the #rules_faq and fill out this little form that will help us in assigning you a role as well as help us find an appropriate time for all to hold the discussions.
-https://rebrand.ly/MKAform
+goo.gl/forms/uLAYS1UfKPUSEwxY2
 
 Summary of rules:
 1. Asking for **medical advice**, whether through DMs or by asking them over here, is **strictly prohibited** and will **not** be tolerated. Any user who does so will be **immediately banned**.
@@ -34,26 +41,16 @@ Summary of rules:
 IF YOU ARE EXPERIENCING A SERIOUS, RAPIDLY WORSENING, OR POTENTIALLY LIFE THREATENING SYMPTOMS, PLEASE CALL *911* OR VISIT THE ER.
 
 By entering this server, you accept that this agreement represents the entire understanding between you and the lead moderators concerning the use of this server.
-If you do accept, please react the ${accept} bellow or you will be **auto-kicked in 24 hours.**
 `
+var dmText = `
+\`\`\`md
+Hello again :)
+This message is to stress the importance of not <asking> or <giving> medical advice on our discord.
+<Note: giving medical consultation over the internet is highly irresponsible and unethical practice, ultimately placing patients in danger>
 
-var ALTwelcomeText = `
-\`\`\`cs
-# Important Information:
-
-You have 10 minutes before you can interact with the members.
-Please take this time to read through the rules and fill out this little form that will help us in assigning you a role as well as help us find an appropriate time for all to hold the discussions.
-
-Summary of rules:
-1. Asking for 'medical advice', whether through DMs or by asking them over here, is 'strictly prohibited' and will 'not' be tolerated. Any user who does so will be 'immediately banned'.
-2. Any member who does 'not' fill the form will be kicked within '24 hours' of joining.
-3. Please be 'respectful of others', and do not use foul language.
-
-IF YOU ARE EXPERIENCING A SERIOUS, RAPIDLY WORSENING, OR POTENTIALLY LIFE THREATENING SYMPTOMS, PLEASE CALL *911* OR VISIT THE ER.
-
-By entering this server, you accept that this agreement represents the entire understanding between you and the lead moderators concerning the use of this server.
+IF YOU <UNDERSTAND> and <ACCEPT> THIS, please type /* "${acceptText}" * (without the quotes)
+IF you do not receive the confirmation response, you will be autokicked in /* 24 hours. *
 \`\`\`
-https://rebrand.ly/MKAform and more in: #rules_faq
 `
 
 var token;
@@ -69,16 +66,30 @@ function autoKick(memberID) {
             if (client.guilds.get(guildID).members.get(memberID).roles.get(rolePending) !== undefined) {
                 console.log("Autokicking: " + mem.user.username + " - " + mem.id);
                 mem.kick("You did not accept the terms and condition (please react tick next time)");
+                kickedCount += 1;
+                console.log("Kicked Count: " + kickedCount);
             } else {
                 console.log(mem.id + ", role manuanly removed");
             }
             notAccepted.splice(i, 1);
         }
     }
-    console.log(notAccepted.length);
+    console.log("Not Accepted Count: " + notAccepted.length);
 }
 
+
+var storedGuild;
 client.on('message', msg => {
+    if (msg.content === "!stats") {
+        msg.reply(
+            `Some stats
+\`\`\`ml
+ - Number Autokicked:   ${kickedCount}
+ - Number Accepted:     ${acceptedCount}
+ - Number Pending :     ${notAccepted.length}
+\`\`\``)
+        msg.delete();
+    }
     if (msg.author.id === devID) {
         // This ID is set to Monacraft's ID
         // Dev Commands
@@ -91,8 +102,11 @@ client.on('message', msg => {
                 msg.reply('Pong!')
             }
         }
-        if (msg.content === '!start') {
+        if (msg.content === '!start' && started === 0) {
             devUser = msg.author;
+            welcomeChannel = msg.channel.id;
+            guildID = msg.guild.id;
+            storedGuild = msg.guild;
             console.log(devUser.username);
             roles = msg.guild.roles.array();
             //console.log(roles);
@@ -107,9 +121,6 @@ client.on('message', msg => {
             msg.delete();
             msg.channel.send("Starting Welcome Log");
         }
-        if (msg.content === "!getrole") {
-            msg.delete();
-        }
         if (msg.content === "!checkrole") {
             console.log(client.guilds.get(guildID).members.get(devID).roles.get(rolePending) !== undefined);
             console.log(client.guilds.get(guildID).members.get(devID).roles.get('21231313'));
@@ -117,20 +128,20 @@ client.on('message', msg => {
         }
         if (msg.content === "!test") {
             console.log('Test Welcome... ' + devUser.username + " with ID: " + devUser.id);
-            var lm = client.user.lastMessage;
-            var thisChannel = lm.channel;
-            client.user.lastMessage.delete();
 
+            var guild = client.guilds.get(guildID);
+            var thisChannel = guild.channels.get(welcomeChannel);
 
             msg.guild.fetchMember(msg.author.id).then(member => {
                 member.addRole(rolePending);
                 notAccepted.push(member.user.id);
-                console.log(notAccepted.length);
+                console.log("Not Accepted Count: " + notAccepted.length);
                 console.log("Would autokick");
             });
             //setTimeout(autoKick, 60000 * 2, member.user.id);
             thisChannel.send(`⭐ Hello ${msg.author.username} and welcome to the Medical Knowledge Association!`);
             thisChannel.send("Welcome Log");
+            msg.author.send(dmText);
             msg.delete();
         }
     }
@@ -141,10 +152,42 @@ client.on('message', msg => {
         if (msg.content === "Starting Welcome Log") {
             started = 1;
             msg.edit(welcomeText + '\nAwaiting New Members...');
+            lastMessageID = msg.id;
         }
         if (msg.content === "Welcome Log") {
-            msg.edit(welcomeText)
-            msg.react(accept);
+            msg.channel.fetchMessage(lastMessageID).then(message => {
+                message.delete();
+            }).catch(err => {
+                console.log(err);
+            })
+            msg.edit(welcomeText);
+            lastMessageID = msg.id;
+        }
+    }
+    if (started === 1 && msg.channel.type === "dm") {
+        var exec = false;
+        if (msg.author.id !== myID) {
+            if (msg.content === acceptText) {
+                exec = true;
+                for (var a = 0; a < notAccepted.length; a++) {
+                    //console.log(r.id + " : " + notAccepted[a]);
+                    if (msg.author.id === notAccepted[a]) {
+                        //var m = react.message.guild.members.get(r[u].id);
+                        client.guilds.get(guildID).fetchMember(msg.author.id).then(member => {
+                            member.removeRole(rolePending);
+                            console.log(member.user.username + " accepted conditions");
+                            member.addRole(roleAccept);
+                            acceptedCount++;
+                            msg.author.send(acceptMsg);
+                        });
+                        notAccepted.splice(a, 1);
+                    }
+                }
+            } else {
+                if (!exec) {
+                    msg.author.send(failMsg);
+                }
+            }
         }
     }
 });
@@ -152,21 +195,28 @@ client.on('guildMemberAdd', member => {
     if (member.guild.id === guildID) {
         if (started === 1) {
             console.log('Welcoming... ' + member.user.username + " with ID: " + member.user.id);
-            var lm = client.user.lastMessage;
-            var thisChannel = lm.channel;
-            client.user.lastMessage.delete();
+
+            var guild = client.guilds.get(guildID);
+            var thisChannel = guild.channels.get(welcomeChannel);
+
             member.addRole(rolePending);
             notAccepted.push(member.user.id);
-            console.log(notAccepted.length);
+            console.log("Not Accepted Count: " + notAccepted.length);
+
             setTimeout(autoKick, 60000 * 60 * 24, member.user.id);
+
             thisChannel.send(`⭐ Hello ${member.user} and welcome to the Medical Knowledge Association!`);
             thisChannel.send("Welcome Log");
+
+            member.user.send(dmText);
+
         } else {
             console.log('Someone joined but I was not started: ' + member.user.username + " : " + member.user.id);
         }
     }
 });
 
+/*
 client.on('messageReactionAdd', react => {
     if (react.message.channel.id === welcomeChannel) {
         if (react.emoji.name === accept) {
@@ -184,11 +234,11 @@ client.on('messageReactionAdd', react => {
                     }
                 }
             });
-            console.log(notAccepted.length);
+            console.log("Not Accepted Count: " + notAccepted.length);
         }
     }
 });
-
+*/
 
 /*
 fs.readFile('..\\MKABot.token', 'utf8', function (err, data) {
